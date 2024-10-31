b809ba6ef3fe869fd14983449abcf3b597fd2709
phtuan2905 <tuannguyen20905@gmail.com>
2024-11-01 00:02:25 +0700

    v.0.0.11

---
+++ JS/NhaThuoc-header.js
@@ -0,0 +1,9 @@
+function showHideDangNhapModal(option) {
+    var dangnhapmodal = document.getElementById("dangnhapmodal");
+    if (option == true) {
+        dangnhapmodal.style.display = "block";
+    }
+    else {
+        dangnhapmodal.style.display = "none";
+    }
+}
